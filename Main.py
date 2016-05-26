b470cb7ef8a8d8ad17666278404d30be7f3421c4
Helithumper <peyton.duncan45@gmail.com>
2016-05-25 22:03:36 -0400

    Initial Import

---
+++ Main.py
@@ -0,0 +1,25 @@
+from prettytable import PrettyTable
+import MySQLdb
+
+
+db = MySQLdb.connect(host='45.55.180.111',user='peyton',passwd='password',db='weather');
+
+cur = db.cursor();
+
+cur.execute("select * from weatherdata order by id desc limit 10");
+print("-----------WEATHER STATION DATA!-----------")
+weatherstationtable = PrettyTable(["ID","Date","Time","Temperature","Windspeed","Sunlight"]);
+#weatherstationtable.set_padding_width(1);
+for row in cur.fetchall():
+    weatherstationtable.add_row(row);
+
+print weatherstationtable;
+
+cur.execute("select * from barometer order by id desc limit 10");
+print("\n\n\n")
+print("--------------BAROMETER DATA!--------------")
+barometertable = PrettyTable(["ID","Date","Time","Barometric Pressure"]);
+#barometertable.set_padding_width(1);
+for row in cur.fetchall():
+    barometertable.add_row(row);
+print barometertable;
